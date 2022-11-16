465a14b2782bcb23c16be3b799de36e7274deb65
oumaimabnz <oumaimabnz1@gmail.com>
2022-11-16 12:41:17 +0100

    question9

--- src/package1/LancerApp1.java
+++ src/package1/LancerApp1.java
@@ -14,35 +14,38 @@ public class LancerApp1 {
 		BufferedImage imgVador= ImageIO.read(new File("images/person2.png"));
 		BufferedImage imgLeila= ImageIO.read(new File("images/person1.png"));
 		
-		// création de la fenêtre de l'application
+		// crï¿½ation de la fenï¿½tre de l'application
 		JFrame laFenetre= new JFrame("Animation Train, etc.");
 		laFenetre.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		laFenetre.setSize(512, 512);
 		
-		// création de la zône de dessin dans la fenêtre
+		// crï¿½ation de la zï¿½ne de dessin dans la fenï¿½tre
 		Dessin d = new Dessin();
 		laFenetre.getContentPane().add(d);
 		
-		// affiche la fenêtre
+		// affiche la fenï¿½tre
 		laFenetre.setVisible(true);
 		
 		// les trains de cercles avec image et en couleur
 		TrainCercle[] lesTrains= new TrainCercle[10];
-		lesTrains[0] = new TrainCercleImage(d, 10, imgVador); d.ajouterObjet(lesTrains[0]);
-		lesTrains[1] = new TrainCercleImage(d, 10, imgLeila); d.ajouterObjet(lesTrains[1]);
+		lesTrains[0] = new TrainCercleImage(d, 10, imgVador); 
+		d.ajouterObjet(lesTrains[0]);
+		lesTrains[1] = new TrainCercleImage(d, 10, imgLeila);
+		d.ajouterObjet(lesTrains[1]);
 		for (int i = 2; i < 10; i++) {
 			lesTrains[i] = new TrainCercleCouleur(new Color((float) Math.random(), (float) Math.random(),
 		(float) Math.random()), d, 10, 10);
 		d.ajouterObjet(lesTrains[i]);
 		}
 		
-		
+		Visage v = new Visage (d);
+		d.ajouterObjet(v);
 		while(true) {
-		// la zone de dessin se réaffiche
+		// la zone de dessin se rï¿½affiche
 		d.repaint();
 		// un temps de pause pour avoir le temps de voir le nouveau dessin
 		d.pause(50);
-		//réaliser à tous les trains un déplacement élémentaire
+		//rï¿½aliser ï¿½ tous les trains un dï¿½placement ï¿½lï¿½mentaire
 		d.animer();
 		
 		}
